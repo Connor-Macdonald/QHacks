4f6a2497b2dfae0844cf640c70d5d815c67da049
unknown <14cm60@queensu.ca>
2018-02-04 07:19:33 -0500

    Added azure text recognition

--- app/src/main/java/com/example/connormacdonald/qhackstest/MainActivity.java
+++ app/src/main/java/com/example/connormacdonald/qhackstest/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,6 +20,9 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImages
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageClassification;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,17 +32,29 @@ import java.util.List;
 import javax.net.ssl.HttpsURLConnection;
 import static java.lang.System.getProperty;
 
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
-    private NaturalLanguageUnderstanding service;
-    private String username = "9235e8e2-26fa-4ae5-aeea-371ff13ce8ff";
-    private String password = "OvItlVOQGuzp";
+    public static final String subscriptionKey = "2368e583599d4695b73b12644065184c";
+    public static final String uriBase = "https://westus.api.cognitive.microsoft.com/vision/v1.0/recognizeText?handwriting=false";
+
     private VisualRecognition vrClient;
     private CameraHelper helper;
     private String BestFoodTag = "na", BestImgTag = "na";
     private Double BestFoodScore = 0d, BestImgScore = 0d;
     private List<String> ClassifierIDS = new ArrayList<String>();
-    private List<String> Catigories = new ArrayList<String>();
 
     public void switchData(View view) {
         TextView detectedObjects = findViewById(R.id.detected_objects);
@@ -105,6 +121,88 @@ public class MainActivity extends AppCompatActivity {
 
     public void openAnalytics(View view) {
 
+    }
+    protected void AzureText(Bitmap BM){
+        HttpClient textClient = new DefaultHttpClient();
+        HttpClient resultClient = new DefaultHttpClient();
+
+        try
+        {
+            // This operation requrires two REST API calls. One to submit the image for processing,
+            // the other to retrieve the text found in the image.
+            //
+            // Begin the REST API call to submit the image for processing.
+            URI uri = new URI(uriBase);
+            HttpPost textRequest = new HttpPost(uri);
+
+            // Request headers. Another valid content type is "application/octet-stream".
+            textRequest.setHeader("Content-Type", "application/octet-stream");
+            textRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            BM.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            InputStreamEntity reqEntity = new InputStreamEntity(inputStream, -1);
+            reqEntity.setContentType("image/jpeg");
+            reqEntity.setChunked(true);
+            textRequest.setEntity(reqEntity);
+
+            // Execute the first REST API call to detect the text.
+            HttpResponse textResponse = textClient.execute(textRequest);
+
+            // Check for success.
+            if (textResponse.getStatusLine().getStatusCode() != 202)
+            {
+                // Format and display the JSON error message.
+                HttpEntity entity = textResponse.getEntity();
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            String operationLocation = null;
+
+            // The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+            Header[] responseHeaders = textResponse.getAllHeaders();
+            for(Header header : responseHeaders) {
+                if(header.getName().equals("Operation-Location"))
+                {
+                    // This string is the URI where you can get the text recognition operation result.
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            // NOTE: The response may not be immediately available. Handwriting recognition is an
+            // async operation that can take a variable amount of time depending on the length
+            // of the text you want to recognize. You may need to wait or retry this operation.
+
+            Log.d("myTag","\nHandwritten text submitted. Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Execute the second REST API call and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = resultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null)
+            {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                Log.d("myTag","Text recognition result response: \n");
+                Log.d("myTag",json.toString(2));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     protected void QueryDatabase(String query) throws Exception {
@@ -241,7 +339,7 @@ public class MainActivity extends AppCompatActivity {
                 final File photoFile = helper.getFile(resultCode);
                 ImageView preview = findViewById(R.id.preview);
                 preview.setVisibility(View.VISIBLE);
-                preview.setImageBitmap(Bitmap.createScaledBitmap(photo, 4096,2303, false));
+                preview.setImageBitmap(Bitmap.createScaledBitmap(photo, 3200,1811, false));
                 AsyncTask.execute(new Runnable() {
                     @Override
                     public void run() {
@@ -266,12 +364,13 @@ public class MainActivity extends AppCompatActivity {
                         }
                         Log.d("MyTag", "Best Tag:" + BestFoodTag);
                         output.append("Item Is: ").append(BestFoodTag).append("\n ");
-
-                        try {
-                            QueryDatabase(BestFoodTag);
-                        }
-                        catch(Exception e)
-                        {
+                        if(false) {
+                            try {
+                                QueryDatabase(BestFoodTag);
+                            } catch (Exception e) {
+                            }
+                        }else{
+                            AzureText(Bitmap.createScaledBitmap(photo, 2000,611, false));
                         }
 
                         /*VisualClassification responseImg =
